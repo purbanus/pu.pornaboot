54d9060d50425a56c018bc64512e6f09d1554784
purbanus <purbanus@gmail.com>
2022-02-10 21:33:59 +0100

    getFiles() -> getFilesPerDirectory()

--- pu.pornaboot/src/main/java/pu/porna/service/impl/PornaServiceImpl.java
+++ pu.pornaboot/src/main/java/pu/porna/service/impl/PornaServiceImpl.java
@@ -7,29 +7,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import pu.porna.bo.FilesDocument;
-import pu.porna.bo.FilesMaker;
+import pu.porna.bo.FilesContainer;
 import pu.porna.bo.RowBounds;
+import pu.porna.dal.Directory;
 import pu.porna.service.PornaService;
 import pu.porna.web.PornaController;
 
+import lombok.Data;
+
 
 @Service
+@Data
 public class PornaServiceImpl implements PornaService
 {
 @SuppressWarnings( "unused" )
 private static final Logger LOG = LoggerFactory.getLogger(PornaController.class);
-@Autowired private FilesMaker filesMaker;
-
-public FilesMaker getFilesMaker()
-{
-	return filesMaker;
-}
+@Autowired private FilesContainer filesContainer;
 
 @Override
-public FilesDocument getFiles( String aDirectory, String aFromFile, RowBounds aRowBounds ) throws IOException
+public Directory getFilesPerDirectory( String aDirectory, String aFromFile, RowBounds aRowBounds ) throws IOException
 {
-	return getFilesMaker().getFiles( aDirectory, aFromFile, aRowBounds );
+	return getFilesContainer().getFilesPerDirectory( aDirectory, aFromFile, aRowBounds );
 }
 
 }

d7df2dd6214c046805e46f5b8a335a9d34626b34
purbanus <purbanus@gmail.com>
2022-02-25 08:22:05 +0100

    Diverse verbeteringen, vooral visueel

--- pu.pornaboot/src/main/java/pu/porna/web/DirectoryRequestParameters.java
+++ pu.pornaboot/src/main/java/pu/porna/web/DirectoryRequestParameters.java
@@ -1,43 +1,16 @@
 package pu.porna.web;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DirectoryRequestParameters
 {
 private Integer pageId;
 private Integer rijen;
 private String zoekenVanaf;
 private String directory;
-public Integer getPageId()
-{
-	return pageId;
-}
-public Integer getRijen()
-{
-	return rijen;
-}
-public String getZoekenVanaf()
-{
-	return zoekenVanaf;
-}
-public String getDirectory()
-{
-	return directory;
-}
-public void setPageId( Integer aPageId )
-{
-	pageId = aPageId;
-}
-public void setRijen( Integer aRijen )
-{
-	rijen = aRijen;
-}
-public void setZoekenVanaf( String aZoekenVanaf )
-{
-	zoekenVanaf = aZoekenVanaf;
-}
-public void setDirectory( String aDirectory )
-{
-	directory = aDirectory;
-}
-
-
 }

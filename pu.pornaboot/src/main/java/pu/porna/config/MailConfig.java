a63e215850c34bb64abbbd81af6cb8c947341f3d
Peter Urbanus <purbanus@gmail.com>
2023-05-21 21:14:01 +0200

    Mail configuratie oppikken uit application.properties

--- pu.pornaboot/src/main/java/pu/porna/config/MailConfig.java
+++ pu.pornaboot/src/main/java/pu/porna/config/MailConfig.java
@@ -1,6 +1,6 @@
 package pu.porna.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.MailMessage;
@@ -8,33 +8,33 @@ import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import lombok.Data;
+
 @Configuration
+@ConfigurationProperties (prefix = "mail")
+@Data
 public class MailConfig
 {
-	@Value( "${porna.mail.host}" )
-	private String mailHost;
-	@Value( "${porna.mail.from}" )
-	private String mailFrom;
-	@Value( "${porna.mail.to}" )
-	private String mailTo;
-	@Value( "${porna.mail.subject}" )
-	private String mailSubject;
+private String host;
+private String from;
+private String to;
+private String subject;
 	
-	@Bean
-	public MailSender mailSender()
-	{
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost( mailHost );
-		return mailSender;
-	}
-	@Bean
-	public MailMessage templateMessage()
-	{
-		MailMessage mailMessage = new SimpleMailMessage();
-		mailMessage.setFrom( mailFrom );
-		mailMessage.setTo( mailTo );
-		mailMessage.setSubject( mailSubject );
-		return mailMessage;
-	}
+@Bean
+public MailSender mailSender()
+{
+	JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	mailSender.setHost( getHost() );
+	return mailSender;
+}
+@Bean
+public MailMessage templateMessage()
+{
+	MailMessage mailMessage = new SimpleMailMessage();
+	mailMessage.setFrom( getFrom() );
+	mailMessage.setTo( getTo() );
+	mailMessage.setSubject( getSubject() );
+	return mailMessage;
+}
 	
 }
